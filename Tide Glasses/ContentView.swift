7d40e6505ea97407aaf3b618da07b99ac670a421
Aahish Abbani <shibe.aahish@gmail.com>
2026-08-02 16:15:40 -0400

    Initial Commit

---
+++ Tide Glasses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tide Glasses
+//
+//  Created by Aahish Abbani on 8/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tide Glasses/Tide_GlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tide_GlassesApp.swift
+//  Tide Glasses
+//
+//  Created by Aahish Abbani on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Tide_GlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
